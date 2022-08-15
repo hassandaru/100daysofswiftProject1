757e1428d7896dddbf8049ede3f75a2f4bb3e8f5
hassandaru <hassandaru@gmail.com>
2022-08-15 13:12:57 +1000

    get image data in viewcontroller using bundle

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fn = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fn.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                //this is a pictrure to load
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+
     }
 
 
