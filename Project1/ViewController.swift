9a1af071b5d927d8a54ff0a5d510a44cae4d3305
hassandaru <hassandaru@gmail.com>
2022-08-15 11:49:34 +1000

    project setup

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Hassan Sohail Dar on 15/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
